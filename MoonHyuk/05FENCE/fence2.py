634d93654298aa8f4085446d9d096b7ec85b587e
MoonHyuk <godmoon00@naver.com>
2016-10-27 20:15:47 +0900

    Update fence2.py

--- MoonHyuk/05FENCE/fence2.py
+++ MoonHyuk/05FENCE/fence2.py
@@ -1,26 +1,26 @@
-#이것도 알수 없는 이유로 시간초과
+#이것도 시간초과
 
-def fence(numb_string):
-    length_of_string = len(numb_string)
+def fence(numbs_li):
+    length_of_string = len(numbs_li)
     if length_of_string == 1:
-        return numb_string
+        return int(numbs_li[0])
     else:
         area_li = []
-        divided_string1 = numb_string[:int(length_of_string / 2)]
-        divided_string2 = numb_string[int(length_of_string / 2):]
-        for i in range(1, min([int(divided_string1[-1]), int(divided_string2[0])]) + 1):
+        divided_li1 = numbs_li[:int(length_of_string / 2)]
+        divided_li2 = numbs_li[int(length_of_string / 2):]
+        for i in range(1, min([int(divided_li1[-1]), int(divided_li2[0])]) + 1):
             count = 2
-            for j in range(2, len(divided_string1) + 1):
-                if int(divided_string1[-j]) >= i: count += 1
+            for j in range(2, len(divided_li1) + 1):
+                if int(divided_li1[-j]) >= i: count += 1
                 else: break
-            for j in range(1, len(divided_string2)):
-                if int(divided_string2[j]) >= i: count += 1
+            for j in range(1, len(divided_li2)):
+                if int(divided_li2[j]) >= i: count += 1
                 else: break
             area_li.append(count * i)
-        area_li.append(int(fence(divided_string1)))
-        area_li.append(int(fence(divided_string2)))
+        area_li.append(fence(divided_li1))
+        area_li.append(fence(divided_li2))
         return max(area_li)
 
 for i in range(0, int(input())):
     ignore_this = input()
-    print(fence(input().replace(" ", "")))
+    print(fence(input().split(" ")))
