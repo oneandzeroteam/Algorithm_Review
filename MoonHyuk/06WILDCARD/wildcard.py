0f2e2db7036ad786eee3a20062e3e0b9646c0192
MoonHyuk <godmoon00@naver.com>
2016-11-04 19:19:17 +0900

    Update wildcard.py

--- MoonHyuk/06WILDCARD/wildcard.py
+++ MoonHyuk/06WILDCARD/wildcard.py
@@ -1,12 +1,10 @@
 # 풀긴 풀었는데 뭔가 편법인거 같은 기분이..
 import re
 
-test_case = int(input())
-for i in range(test_case):
+for i in range(int(input())):
     re_string = input().replace('?', '.').replace('*', '\w*')
     string_list = []
-    numbs_string = int(input())
-    for j in range(numbs_string):
+    for j in range(int(input())):
         input_string = input()
         if re.match(re_string + '$', input_string):
             string_list.append(input_string)
