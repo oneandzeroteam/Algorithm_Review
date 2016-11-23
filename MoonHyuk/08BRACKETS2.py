133ee83a33dad69bfbbce407ce83ed7201b03e65
MoonHyuk <godmoon00@naver.com>
2016-11-24 03:30:54 +0900

    Create 08BRACKETS2.py

---
+++ MoonHyuk/08BRACKETS2.py
@@ -0,0 +1,20 @@
+def brackets2(input_string):
+    li = []
+    for i, j in enumerate(input_string):
+        if j == '(' or j == '[' or j == '{':
+            li.append(j)
+
+        else:
+            if len(li) == 0:
+                return "NO"
+
+            elif (j == ')' and li[-1] == '(') or (j == ']' and li[-1] == '[') or (j == '}' and li[-1] == '{'):
+                li.pop()
+
+            else:
+                return "NO"
+
+    return "YES" if len(li) == 0 else "NO"
+
+for _ in range(int(input())):
+    print(brackets2(input()))
